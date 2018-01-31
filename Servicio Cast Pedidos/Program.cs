efe34ab0748831fe3f9fb77648077bcbd0116682
Greg Perez <gperez@SRV-HANASOP.exxis.local>
2018-01-30 22:32:54 -0300

    actualizando repositorio real

---
+++ Servicio Cast Pedidos/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Servicio_Cast_Pedidos.Servicios;
+using System.Reflection;
+
+namespace Servicio_Cast_Pedidos
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] servicesToRun;
+            servicesToRun = new ServiceBase[]
+            {
+                new Pedidos()
+            };
+            ServiceBase.Run(servicesToRun);
+            /*if (Environment.UserInteractive)
+            {
+                RunInteractive(servicesToRun);
+            }
+            else
+            {
+                ServiceBase.Run(servicesToRun);
+            }*/
+        }
+
+        private static void RunInteractive(ServiceBase[] servicesToRun)
+        {
+            MethodInfo onStartMethod = typeof(ServiceBase).GetMethod("OnStart",
+            BindingFlags.Instance | BindingFlags.NonPublic);
+            foreach (ServiceBase service in servicesToRun)
+            {
+                Console.Write("Starting {0}...", service.ServiceName);
+                onStartMethod.Invoke(service, new object[] { new string[] { } });
+                Console.Write("{0} Started", service.ServiceName);
+            }
+
+            Console.WriteLine("Press any key to stop the service");
+            Console.Read();
+            Console.WriteLine();
+
+            MethodInfo onStopMethod = typeof(ServiceBase).GetMethod("OnStop",
+            BindingFlags.Instance | BindingFlags.NonPublic);
+            foreach (ServiceBase service in servicesToRun)
+            {
+                Console.Write("Stopping {0}...", service.ServiceName);
+                onStopMethod.Invoke(service, null);
+                Console.WriteLine("{0} Stopped", service.ServiceName);
+            }
+            //WThread.Sleep(1000);
+        }
+    }
+}
